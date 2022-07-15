55a6da25687450321d4cab5223d1ac3783ff3d75
donus <donus33@gmail.com>
2022-07-15 15:27:02 +0700

    update

--- config.h
+++ config.h
@@ -1,20 +1,14 @@
 #pragma once
 
 /* Select hand configuration */
-#define MASTER_LEFT
-#define USE_SERIAL_PD2
+// #define MASTER_RIGHT
+#define EE_HANDS
+// #define USE_SERIAL_PD2
 // #define SSD1306OLED
 
 // Pick good defaults for enabling homerow modifiers
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
 // #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 200
+#define TAPPING_TERM 180
 
-// Underglow
-/*
-#undef RGBLED_NUM
-#define RGBLED_NUM 14    // Number of LEDs
-#define RGBLIGHT_ANIMATIONS
-#define RGBLIGHT_SLEEP
-*/
--- keymap.c
+++ keymap.c
@@ -1,31 +1,53 @@
 #include QMK_KEYBOARD_H
 
+enum layer_number {
+  _QWERTY = 0,
+  _NUM_SYM,
+  _FUNC,
+  _NAV,
+  _MOUSE,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT(
+    /* QWERTY
+     * ,-----------------------------------------.                    ,-----------------------------------------.
+     * |      |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
+     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+     * |      |   Q  |  W   |  E   |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |      |
+     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+     * |      |lsft A|lctl S|lalt D|lgui F|   G  |-------.    ,-------|   H  |rgui J|ralt K|rctl L|rsft ;|      |
+     * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+     * |      |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |      |
+     * `-----------------------------------------/       /     \      \-----------------------------------------'
+     *                   |      |      | L1   | / L3    /       \L4    \  |   L2  |      |      |
+     *                   |      |      |      |/ Space /         \Enter \ | Backsp|      |      |
+     *                   `----------------------------'           '------''--------------------'
+     */
+  [_QWERTY] = LAYOUT(
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_TRNS,
     KC_TRNS,LSFT_T(KC_A),LCTL_T(KC_S),LALT_T(KC_D),LGUI_T(KC_F),KC_G,                     KC_H,RGUI_T(KC_J),RALT_T(KC_K),RCTL_T(KC_L),RSFT_T(KC_SCLN),KC_TRNS,
     KC_TRNS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_TRNS,              KC_TRNS,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_TRNS,
-    KC_TRNS, KC_TRNS, MO(1), LT(3,KC_SPC),                                                LT(4,KC_ENT), LT(2,KC_BSPC), KC_TRNS, KC_TRNS),
-  [1] = LAYOUT( // numbers and symbols
+    KC_TRNS, KC_TRNS, MO(_NUM_SYM), LT(_NAV,KC_SPC),                                     LT(_MOUSE,KC_ENT), LT(_FUNC,KC_BSPC), KC_TRNS, KC_TRNS),
+  [_NUM_SYM] = LAYOUT( // numbers and symbols
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                 KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_QUOT, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_TRNS, KC_BSPC, KC_GRV,  KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-  [2] = LAYOUT( // functions
+  [_FUNC] = LAYOUT( // functions
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
-    KC_TRNS, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPRV, KC_TRNS,                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F11,  KC_TRNS,
-    KC_TRNS, KC_MUTE, KC_MPLY, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F12,  KC_TRNS,
+    KC_TRNS, KC_TAB,  KC_TRNS, KC_VOLD, KC_VOLU, KC_TRNS,                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F11,  KC_TRNS,
+    KC_TRNS, KC_MUTE, KC_MPLY, KC_MNXT, KC_MPRV, KC_DEL,  KC_TRNS,               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F12,  KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TAB, KC_ESC,                                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-  [3] = LAYOUT( // navigation
+  [_NAV] = LAYOUT( // navigation
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                 KC_TRNS, KC_END, KC_HOME, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                 KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                 KC_PGUP, KC_END,  KC_HOME, KC_PGDN, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                 KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-  [4] = LAYOUT( // mouse
+  [_MOUSE] = LAYOUT( // mouse
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                 KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2, KC_BTN3,                                 KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS, KC_TRNS,
